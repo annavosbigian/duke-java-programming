46e40d0c3275ee9e6472b85786ea2f34971fe78e
annavosbigian <52043723+annavosbigian@users.noreply.github.com>
2019-10-09 15:51:54 -0700

    Create MarkovRunnerWithInterface.java

---
+++ Principles of Software Design/Week 3/MarkovRunnerWithInterface.java
@@ -0,0 +1,69 @@
+
+/**
+ * Write a description of class MarkovRunner here.
+ * 
+ * @author Duke Software
+ * @version 1.0
+ */
+
+import edu.duke.*; 
+
+public class MarkovRunnerWithInterface {
+    public void runModel(IMarkovModel markov, String text, int size, int seed) {
+        markov.setTraining(text);        
+        markov.setRandom(seed);
+        String m = markov.toString().substring(5);
+        System.out.println("MarkovModel of order " + m);
+        for(int k=0; k < 3; k++){
+            String st= markov.getRandomText(size);
+            printOut(st);
+        }
+    }
+    
+    public void runMarkov() {
+        FileResource fr = new FileResource();
+        String st = fr.asString();
+        st = st.replace('\n', ' ');
+        int size = 200;
+        int seed = 5;
+        
+        MarkovZero mz = new MarkovZero();
+        runModel(mz, st, size, seed);
+    
+        MarkovOne mOne = new MarkovOne();
+        runModel(mOne, st, size, seed);
+        
+        MarkovModel mThree = new MarkovModel(3);
+        runModel(mThree, st, size, seed);
+        
+        MarkovFour mFour = new MarkovFour();
+        runModel(mFour, st, size, seed);
+
+    }
+
+    private void printOut(String s){
+        String[] words = s.split("\\s+");
+        int psize = 0;
+        System.out.println("----------------------------------");
+        for(int k=0; k < words.length; k++){
+            System.out.print(words[k]+ " ");
+            psize += words[k].length() + 1;
+            if (psize > 60) {
+                System.out.println();
+                psize = 0;
+            }
+        }
+        System.out.println("\n----------------------------------");
+    }
+    
+    public void testHashMap(){
+    EfficientMarkovModel mark = new EfficientMarkovModel(2);
+    String training = "yes-this-is-a-thin-pretty-pink-thistle";
+    int size = 50;
+    int seed = 42;
+    mark.setTraining(training);
+    mark.setRandom(42);
+    System.out.println(mark.buildMap());
+    System.out.println(mark.getRandomText(size));
+    }
+}
