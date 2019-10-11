cc0118d9f77edb69c6789f369257ac5f1a21f12c
annavosbigian <52043723+annavosbigian@users.noreply.github.com>
2019-10-11 13:00:16 -0700

    Create MarkovRunner.java

---
+++ Principles of Software Design/Week 3/MarkovRunner.java
@@ -0,0 +1,65 @@
+
+/**
+ * Write a description of class MarkovRunner here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+
+import edu.duke.*;
+
+public class MarkovRunner {
+    public void runModel(IMarkovModel markov, String text, int size){ 
+        markov.setTraining(text); 
+        System.out.println("running with " + markov); 
+        for(int k=0; k < 3; k++){ 
+            String st = markov.getRandomText(size); 
+            printOut(st); 
+        } 
+    } 
+
+    public void runModel(IMarkovModel markov, String text, int size, int seed){ 
+        markov.setTraining(text); 
+        markov.setRandom(seed);
+        System.out.println("running with " + markov); 
+        for(int k=0; k < 3; k++){ 
+            String st = markov.getRandomText(size); 
+            printOut(st); 
+        } 
+    } 
+
+    public void runMarkov() { 
+        FileResource fr = new FileResource(); 
+        String st = fr.asString(); 
+        st = st.replace('\n', ' '); 
+        //MarkovWordOne markovWord = new MarkovWordOne(); 
+        //runModel(markovWord, st, 200); 
+    } 
+
+    public void testHashMap(){
+        EfficientMarkovWord emw = new EfficientMarkovWord(2);
+        emw.setRandom(42);
+        FileResource fr = new FileResource();
+        String file = fr.asString();
+        emw.setTraining(file);
+        String text = emw.getRandomText(50);
+        System.out.println(text);
+        System.out.println(System.nanoTime());
+    }
+    
+    private void printOut(String s){
+        String[] words = s.split("\\s+");
+        int psize = 0;
+        System.out.println("----------------------------------");
+        for(int k=0; k < words.length; k++){
+            System.out.print(words[k]+ " ");
+            psize += words[k].length() + 1;
+            if (psize > 60) {
+                System.out.println(); 
+                psize = 0;
+            } 
+        } 
+        System.out.println("\n----------------------------------");
+    } 
+
+}
